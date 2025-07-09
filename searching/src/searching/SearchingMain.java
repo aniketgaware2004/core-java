6964c6711d222c50e54437654fc47f6640b8ec14
aniketgaware2004 <aniketgaware.work@gmail.com>
2025-07-09 11:27:42 -0700

    Binary Search by Recursion.

--- searching/src/searching/SearchingMain.java
+++ searching/src/searching/SearchingMain.java
@@ -23,6 +23,21 @@ public class SearchingMain {
 	}
 	    return -1;
 	}
+	//binary search through recurssion.
+	public static int recBinarySearch(int [] arr, int left, int right, int key){
+		int index;
+		int mid=(left+right)/2;
+		if(right<left)
+			return -1;
+		if(key==arr[mid])
+			return mid;
+		if(key< arr[mid])
+			index=recBinarySearch(arr,left,mid-1,key);
+		else
+			index=recBinarySearch(arr,mid+1,right,key);
+		return index;
+		
+	}
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
 		int [] arr= {10, 20, 30, 40, 50, 460};
@@ -41,6 +56,12 @@ public class SearchingMain {
 			System.out.println("key found at index: "+ index);
 		else
 			System.out.println("key not found.");
+		
+		index=recBinarySearch(arr,0,arr.length-1,key);
+		if(index!=-1)
+			System.out.println("key found at index: "+index);
+		else
+			System.out.println("key not found");
 		
 	}
 
