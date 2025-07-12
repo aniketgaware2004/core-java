5f1887c446f82453d130f0441de7e6d9f65023b8
aniketgaware2004 <aniketgaware.work@gmail.com>
2025-07-12 10:11:13 -0700

    Implementation of an Insertion sort.

--- MoreImpBubbleSort.java
+++ MoreImpBubbleSort.java
@@ -13,16 +13,32 @@ public class MoreImpBubbleSort {
 					swapflag= true;
 				}
 			}
-			if(!swapflag)
+			if(!swapflag)  
 				break;
 		}
-	}
+		
+		public static void insertionSort(int [] arr) {
+			//n-1 passes
+			for(int  i=1; i< arr.length ; i++) {
+				//COPY LAST ELEMENT INTO TEMP VARIABLE
+				int j, temp=arr[i];
+				for( j=i-1; j>=0 && arr[j]> temp; j--) {
+				arr[j+1] = arr[j];
+			}
+			arr[j+1]= temp;
+			}
+		}
+	
 		
 		public static void main(String[] args) {
 			int [] arr= {6,4,2,8,3,1};
 			System.out.println("Before:"+Arrays.toString(arr));
-			moreImpbubblesort(arr);
+			//moreImpbubblesort(arr);
+			//System.out.println("After:"+Arrays.toString(arr));
+			
+			insertionSort(arr);										
 			System.out.println("After:"+Arrays.toString(arr));
+			
 		}
 
 }
